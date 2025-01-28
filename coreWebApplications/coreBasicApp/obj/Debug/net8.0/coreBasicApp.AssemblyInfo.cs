4da155093cc2edc7fc442237e8e98d09c695992b
bgunwani <bhawna.gunwani@gmail.com>
2025-01-28 14:17:48 +0530

    Secomd Commit

--- coreWebApplications/coreBasicApp/obj/Debug/net8.0/coreBasicApp.AssemblyInfo.cs
+++ coreWebApplications/coreBasicApp/obj/Debug/net8.0/coreBasicApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("coreBasicApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d7510c6c9920c1ae43511d3cdf2051df6d3b193")]
 [assembly: System.Reflection.AssemblyProductAttribute("coreBasicApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("coreBasicApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- coreWebApplications/coreMvcApp/obj/Debug/net8.0/coreMvcApp.AssemblyInfo.cs
+++ coreWebApplications/coreMvcApp/obj/Debug/net8.0/coreMvcApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("coreMvcApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d7510c6c9920c1ae43511d3cdf2051df6d3b193")]
 [assembly: System.Reflection.AssemblyProductAttribute("coreMvcApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("coreMvcApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
